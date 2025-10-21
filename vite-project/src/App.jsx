a72b2ecb6fd899ad69ef41d37fc42c22db8e0a77
AbdullahMohzar <abdullahmohzar@gmail.com>
2025-10-21 06:37:32 +0500

    sprint 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css'
-import HostelDetails from './pages/HostelDetails';
+import HostelDetails from './pages/HostelDetails.jsx';
+import Login from './pages/Login.jsx';
+import OwnerDashboard from '/pages/OwnerDashboard.jsx';
+import UserDashboard from './pages/UserDashboard.jsx';
+import Home from'./pages/Home.jsx';
+import Signup from './pages/Signup.jsx';
+
 
 function App() {
   
